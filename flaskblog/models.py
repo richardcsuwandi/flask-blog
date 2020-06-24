0a7d0dfc872910f516fff30daa7e799ea18765c8
richardcsuwandi <59959022+richardcsuwandi@users.noreply.github.com>
2020-06-24 13:29:33 +0700

    Update models.py

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -9,7 +9,6 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
