7f81392fdd0f546d6db945a5435cadfe43150103
richardcsuwandi <59959022+richardcsuwandi@users.noreply.github.com>
2020-06-28 19:58:25 +0700

    Update forms.py

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -39,7 +39,7 @@ class UpdateAccountForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField("Email",
                         validators=[DataRequired(), Email()])
-    picture = FileField("Update Profile Picture", validators=[FileAllowed(["jpg", "jpeg", "png"])])
+    picture = FileField("Update Profile Picture", validators=[FileAllowed(["jpg", "png"])])
     submit = SubmitField("Update")
 
     def validate_username(self, username):
